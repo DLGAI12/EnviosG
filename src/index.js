a5d95a958fb69a38472ff860a4c9dcd76be276f6
DLGAI12 <113869471+DLGAI12@users.noreply.github.com>
2024-11-02 22:42:28 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-const express = require("express")
-
-const app = express()
-const morgan = require("morgan")
-const taskRoutes = require('./routes/tasks.routes.js')
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(taskRoutes)
-app.listen(4000)
-app.use((err, req, res, next) => {
-    res.status(404).json({
-        message: err.message
-    })
-})
+const express = require('express');
+const app = express();
+app.listen(3000)
+console.log("Hola")
