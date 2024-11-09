9891078bc9f3cbf4e1b351870ea231527545cb10
DLGAI12 <adrian.deleon0252@alumnos.udg.mx>
2024-11-08 18:20:51 -0600

    Cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require("express")
 
 const app = express()
+const cors = require('cors');
 const morgan = require("morgan")
 const taskRoutes = require('./routes/tasks.routes.js')
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(taskRoutes)
 app.listen(4000)
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 app.use((err, req, res, next) => {
     res.status(404).json({
         message: err.message
