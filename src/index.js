837681d4c313a3b0aa9c9dbdac5e1c346d1026bf
DLGAI12 <adrian.deleon0252@alumnos.udg.mx>
2024-11-08 18:56:44 -0600

    Cors 2.0

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ const app = express()
 const cors = require('cors');
 const morgan = require("morgan")
 const taskRoutes = require('./routes/tasks.routes.js')
+const corsOptions = {
+    origin: 'http://localhost:5173', // Aquí pones la URL de tu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
+};
+
+app.use(cors(corsOptions)); // Habilita CORS con las opciones que configuraste
+
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(taskRoutes)
 app.listen(4000)
-const corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+
 
 app.use((err, req, res, next) => {
     res.status(404).json({
